7e59e679bcec449668fc0b011f3c4058519bcd47
Oleks Chemerys <Oleks.Chemerys@safetycodes.ab.ca>
2025-02-25 14:17:23 -0700

    refactoring House List tests to mock api calls

--- src/components/__tests__/HouseList.test.tsx
+++ src/components/__tests__/HouseList.test.tsx
@@ -1,29 +1,43 @@
 import {
-  render, screen, within, fireEvent,
+  render, screen, within, fireEvent, waitFor,
 } from '@testing-library/react';
+import { MockedFunction } from 'vitest';
 import HouseList from '../HouseList';
 import currencyFormatter from '../../utils/currencyFormatter';
 
 describe('HouseLst component', () => {
+  const apiUrl = 'http://localhost:3000/houses';
+  const mockData = [
+    {
+      id: 1,
+      address: '12 Valley of Kings, Geneva',
+      country: 'Switzerland',
+      price: 900000,
+    },
+    {
+      id: 2,
+      address: '89 Road of Forks, Bern',
+      country: 'Switzerland',
+      price: 500000,
+    },
+  ];
+
   beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
+  it('fetches and displays data on mount', async () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: vi.fn().mockResolvedValue([...mockData]),
+    });
+
     render(<HouseList />);
-  });
 
-  it('renders HouseLIst title', () => {
+    // Check if the title is rendered
     expect(screen.getByText('Houses currently on the market')).toBeInTheDocument();
-  });
-
-  it('renders House List table', () => {
     // Check if the table is rendered
     const tableElement = screen.getByRole('table');
     expect(tableElement).toBeInTheDocument();
-  });
-
-  it('renders House List table with correct column names', () => {
-    // Check if the table is rendered
-    const tableElement = screen.getByRole('table');
-    expect(tableElement).toBeInTheDocument();
-
     // Check for specific column names within the table header
     const columnNames = ['Address', 'Country', 'Asking Price'];
     const headers = within(tableElement).getAllByRole('columnheader');
@@ -31,61 +45,72 @@ describe('HouseLst component', () => {
     columnNames.forEach((name) => {
       expect(headerTexts).toContain(name);
     });
-  });
-
-  it('renders House List table with values', () => {
-    const data = [
-      {
-        id: 1,
-        address: '12 Valley of Kings, Geneva',
-        country: 'Switzerland',
-        price: 900000,
-      },
-      {
-        id: 2,
-        address: '89 Road of Forks, Bern',
-        country: 'Switzerland',
-        price: 500000,
-      },
-    ];
-
+    // check if correct request is made
+    await waitFor(() => expect(global.fetch).toHaveBeenCalledWith(apiUrl));
     // fetching data
-    setTimeout(() => {
-      const rows = screen.getAllByRole('row');
-      expect(rows).toHaveLength(data.length + 1); // Including header row
+    const rows = screen.getAllByRole('row');
+    expect(rows).toHaveLength(2 + 1); // Including header row
 
-      data.forEach((item, index) => {
-        const cells = within(rows[index + 1]).getAllByRole('cell');
-        expect(cells[0]).toHaveTextContent(item.address);
-        expect(cells[1]).toHaveTextContent(item.country);
-        expect(cells[2]).toHaveTextContent(currencyFormatter.format(item.price));
-      });
-    }, 100);
+    mockData.forEach((item, index) => {
+      const cells = within(rows[index + 1]).getAllByRole('cell');
+      expect(cells[0]).toHaveTextContent(item.address);
+      expect(cells[1]).toHaveTextContent(item.country);
+      expect(cells[2]).toHaveTextContent(currencyFormatter.format(item.price));
+    });
   });
 
-  it('adds new row when the button "Add" is clicked', () => {
+  it('adds new row when the button "Add" is clicked', async () => {
     const newRowData = {
       id: 3,
       address: '123 Main St. Edmonton',
       country: 'Canada',
       price: 100000,
     };
+    // api call response stubs
+    global.fetch = vi.fn()
+      .mockResolvedValueOnce({
+        // get items response
+        json: vi.fn().mockResolvedValue([]),
+      })
+      .mockResolvedValueOnce({
+        // create new item response
+        json: vi.fn().mockResolvedValue(newRowData),
+      });
 
-    const rows = screen.getAllByRole('row');
-    const rowsCount = rows.length;
-
+    render(<HouseList />);
     const button = screen.getByRole('button', { name: 'Add' });
+    expect(button).toBeInTheDocument();
+    // check if correct request is made
+    await waitFor(() => expect(global.fetch).toHaveBeenCalledWith(apiUrl));
+
+    // just only header
+    expect(screen.getAllByRole('row')).toHaveLength(1);
+
     fireEvent.click(button);
 
-    setTimeout(() => {
-      const newRows = screen.getAllByRole('row');
-      expect(newRows).toHaveLength(rowsCount + 1);
+    await waitFor(() => {
+      const lastCall = (global.fetch as MockedFunction<typeof fetch>).mock.calls.at(-1);
+      if (!lastCall) throw new Error('No API calls found');
 
-      // last row
-      const cells = within(newRows[rowsCount]).getAllByRole('cell');
-      expect(cells[0]).toHaveTextContent(newRowData.address);
-      expect(cells[1]).toHaveTextContent(newRowData.country);
-      expect(cells[2]).toHaveTextContent(currencyFormatter.format(newRowData.price));
-    }, 100);
+      const body = lastCall[1]?.body;
+      if (typeof body !== 'string') {
+        throw new Error(`Expected body to be a string but got ${typeof body}`);
+      }
+
+      const requestBody = JSON.parse(body);
+      expect(requestBody.address).toBe(newRowData.address);
+      expect(requestBody.country).toBe(newRowData.country);
+      expect(requestBody.price).toBe(newRowData.price);
+    });
+
+    const rowsCount = 1 + 1; // one header row and one data row
+    const newRows = screen.getAllByRole('row');
+    expect(newRows).toHaveLength(rowsCount);
+
+    // last row cells
+    const cells = within(newRows[1]).getAllByRole('cell');
+    expect(cells[0]).toHaveTextContent(newRowData.address);
+    expect(cells[1]).toHaveTextContent(newRowData.country);
+    expect(cells[2]).toHaveTextContent(currencyFormatter.format(newRowData.price));
   });
 });
