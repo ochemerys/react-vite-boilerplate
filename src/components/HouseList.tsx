a835887c7a6c975a840eacdeaef0b4eb9974f28b
Oleks Chemerys <Oleks.Chemerys@safetycodes.ab.ca>
2025-02-27 11:54:33 -0700

    refactoring: implemt custom hook

--- src/components/HouseList.tsx
+++ src/components/HouseList.tsx
@@ -1,32 +1,12 @@
-import { useState, useEffect } from 'react';
 import HouseListRow from './HouseListRow';
-import { IHouse } from '../types/IHouse';
 import { HouseListProps } from './interfaces';
-import { fetchHouses, createHouse } from '../api/houses-api';
+import { createHouse } from '../api/houses-api';
+import useHouses from '../hooks/useHouses';
 
 function HouseList(props: HouseListProps) {
   const { selectHouse } = props;
-  // state hook
-  const [houses, setHouses] = useState([] as IHouse[]);
 
-  // effect hook
-  useEffect(() => {
-    // does not support async function as parameter
-    // that's why we need async getHouses function
-    const getHouses = async () => {
-      const houseArray = await fetchHouses();
-      setHouses(houseArray);
-    };
-    getHouses();
-  }, []); // add empty dependency array: we have no dependenicies
-
-  // memo hook: cache component houses if houses array is not changed
-  // memo should not used blindly - it has side effects
-  /*
-    const result = useMemo(() => {
-      return timeConsumingCalculation(houses)
-    }, [houses]);
-  */
+  const { houses, setHouses } = useHouses();
 
   function addHouse() {
     const newHouse = {
